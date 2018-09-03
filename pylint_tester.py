ecedf2d54f36afc59d45ecded781f2423ee63e40
rudiscarpa96 <rudiscarpa96@gmail.com>
2018-09-03 21:18:49 +1000

    -initial pylint testing

---
+++ pylint_tester.py
@@ -0,0 +1,25 @@
+from pylint.lint import Run
+from pylint.reporters.text import TextReporter
+
+class WritableObject:
+
+    def __init__(self):
+        self._content = []
+
+    def write(self, st):
+        self._content.append(st)
+
+    def read(self):
+        return self._content
+
+
+def run_pylint(filename='C:/Users/Rudi/Documents/Uni/CSSE1001/2018s2a1/a1.py'):
+    ARGS = []
+    pylint_output = WritableObject()
+    Run([filename] + ARGS, reporter=TextReporter(pylint_output), do_exit=False)
+
+    for x in pylint_output.read():
+        print(x, end='')
+
+if __name__ == '__main__':
+    run_pylint()
